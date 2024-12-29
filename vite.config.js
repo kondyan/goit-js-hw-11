d256f9bd07b8fb5760cae0fdd939882dfee91312
kondyan <arsenij.kondratenko@gmail.com>
2024-12-29 23:46:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Configuration options go here
-  base: "goit-js-hw-11", // Base public path when deploying
+  base: "/goit-js-hw-11/", // Base public path when deploying
   plugins: [], // Add any Vite plugins here
 });
