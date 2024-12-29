054ac5ccf4ea452c6006740c63170ebbdbb14019
kondyan <arsenij.kondratenko@gmail.com>
2024-12-29 23:35:10 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Configuration options go here
+  base: "/", // Base public path when deploying
+  plugins: [], // Add any Vite plugins here
+});
